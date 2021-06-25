b9bcb42b17c36739c3a5cd6c0df5297eabce6d30
Charlie Kemp <31106448+hello-ck@users.noreply.github.com>
2021-06-25 10:17:32 -0400

    gripper & navigation cameras at same time (low resolution)
    This commit enables the wide-angle gripper camera and the
    wide-angle navigation camera to function at the same time.
    However, this only works with 320x240 resolution at 30
    frames per second for both of the cameras.
    
    The new installation script configures the uvcvideo kernel
    module to use the following two quirks:
    
      UVC_QUIRK_RESTRICT_FRAME_RATE
      UVC_QUIRK_FIX_BANDWIDTH
    
    The low video resolution and quirks avoid the following error
    due to USB 2 bandwidth limits:
    
      VIDIOC_STREAMON error 28, No space left on device
    
    More information can be found at the following two websites:
    
      http://www.ideasonboard.org/uvc/faq/
      http://www.ideasonboard.org/uvc/faq/

---
+++ bash_scripts/install_gripper_and_navigation_cameras.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo ""
+echo "Starting gripper and navigation camera installation script."
+
+# UDEV FOR GRIPPER CAMERA
+echo ""
+echo "Adding udev rules for the gripper camera."
+echo "cd ~/catkin_ws/src/stretch_web_interface/"
+cd ~/catkin_ws/src/stretch_web_interface/
+echo "sudo cp ./89-hello-gripper-camera.rules /etc/udev/rules.d/"
+sudo cp ./89-hello-gripper-camera.rules /etc/udev/rules.d/
+echo "sudo udevadm control --reload"
+sudo udevadm control --reload
+echo "WARNING: You should either reboot the robot or disconnect and reconnect the gripper camera for the new device symlink hello-gripper-camera to take effect."
+echo ""
+
+# UDEV FOR NAVIGATION CAMERA
+echo ""
+echo "Adding udev rules for the navigation camera."
+echo "cd ~/catkin_ws/src/stretch_web_interface/"
+cd ~/catkin_ws/src/stretch_web_interface/
+echo "sudo cp ./88-hello-navigation-camera.rules /etc/udev/rules.d/"
+sudo cp ./88-hello-navigation-camera.rules /etc/udev/rules.d/
+echo "sudo udevadm control --reload"
+sudo udevadm control --reload
+echo "WARNING: You should either reboot the robot or disconnect and reconnect the navigation camera for the new device symlink hello-navigation-camera to take effect."
+echo ""
+
+# ROS USB CAMERA PACKAGE
+echo ""
+echo "Installing ROS USB camera package"
+echo "sudo apt-get --yes install ros-melodic-usb-cam"
+sudo apt-get --yes install ros-melodic-usb-cam
+echo "Done."
+
+# CHANGE UVCVIDEO SETTINGS TO PERMIT BOTH CAMERAS TO WORK AT THE SAME TIME AT LOW RESOLUTION (320x240)
+echo ""
+echo "Change default uvcvideo kernal module settings to enable two quirks: UVC_QUIRK_RESTRICT_FRAME_RATE and UVC_QUIRK_FIX_BANDWIDTH."
+echo "cd ~/catkin_ws/src/stretch_web_interface/"
+cd ~/catkin_ws/src/stretch_web_interface/
+echo "sudo cp ./uvcvideo.conf /etc/modprobe.d/"
+sudo cp ./uvcvideo.conf /etc/modprobe.d/
+echo "sudo rmmod uvcvideo"
+sudo rmmod uvcvideo
+echo "sudo modprobe uvcvideo"
+sudo modprobe uvcvideo
+
+echo ""
+echo "The gripper and navigation camera installation script has finished."
+echo ""
